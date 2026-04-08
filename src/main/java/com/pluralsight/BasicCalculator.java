69bb1560b049d698bfa525087bce8f0dc48d478f
branRoberts <brandonkrob12@gmail.com>
2026-04-08 14:18:17 -0400

    Second Commit I made comments to the code explaining what I did in each line

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class BasicCalculator {
     public static void main(String[] args) {
         Scanner inputNumber = new Scanner(System.in);
-
+// this line above creates the input for future use and calling it inputNumber
         System.out.print("Enter first number: ");
         int firstNumber = inputNumber.nextInt();
-
+// the two lines above asks the user to put input a number then stores the input in variable firstNumber
         System.out.print("Enter second number: ");
         int secondNumber = inputNumber.nextInt();
-
+// the two lines above asks the user to put input a number then stores the input in variable secondNumber
         int result = firstNumber * secondNumber;
-
+// this multiplies the two inputs in the variables and stores in the result variable
         System.out.println("The result is: " + result);
         inputNumber.close();
-
+// the two lines above prints out the result and then closes the scanner function
 
     }
 }
